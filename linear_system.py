363ab17c2ff6db17e334832845a217566ba7dc1b
Piyush Malhotra <piyushmalhotra28@gmail.com>
2017-07-20 16:59:23 +0530

    Add indexing capability

--- linear_system.py
+++ linear_system.py
@@ -15,3 +15,44 @@ class LinearSystem(object):
             self.dimension = d
         except AssertionError:
             raise Exception(self.ALL_PLANES_MUST_BE_IN_SAME_DIM_MSG)
+
+    def indices_of_first_nonzero_terms_in_each_row(self):
+        num_equations = len(self)
+        num_variables = self.dimension
+
+        indices = [-1] * num_equations
+
+        for i,p in enumerate(self.planes):
+            try:
+                indices[i] = p.first_nonzero_index(p.normal_vector.coordinates)
+            except Exception as e:
+                if str(e) == Plane.NO_NONZERO_ELTS_FOUND_MSG:
+                    continue
+                else:
+                    raise e
+
+        return indices
+
+    def __len__(self):
+        return len(self.planes)
+
+    def __getitem__(self, i):
+        return self.planes[i]
+
+    def __setitem__(self, i, x):
+        try:
+            assert x.dimension == self.dimension
+            self.planes[i] = x
+
+        except AssertionError:
+            raise Exception(self.ALL_PLANES_MUST_BE_IN_SAME_DIM_MSG)
+
+    def __str__(self):
+        ret = 'Linear System:\n'
+        temp = ['Equation {}: {}'.format(i+1,p) for i,p in enumerate(self.planes)]
+        ret += '\n'.join(temp)
+        return ret
+
+    @staticmethod
+    def is_near_zero(val, eps=1e-10):
+        return abs(val) < eps
